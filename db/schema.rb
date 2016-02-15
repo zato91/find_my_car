df377422c9d85d1ee2e0b49674a6e26bdf877a75
zara <zara59@msn.com>
2016-02-15 18:54:32 +0100

    change name car column adress to address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215150842) do
+ActiveRecord::Schema.define(version: 20160215175211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160215150842) do
     t.string   "brand"
     t.text     "description"
     t.integer  "price"
-    t.string   "adress"
+    t.string   "address"
     t.string   "engin"
     t.integer  "year"
     t.integer  "km"
