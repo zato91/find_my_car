5ba108e7ed3c7a1ae04da5016a011a2b7f44b328
dheidet <dimitri@36quaidesaromes.fr>
2016-02-16 16:23:36 +0100

    facebook sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
-
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'pages#home'
 
   resources :cars, only: [:index, :new, :create, :show] do
