ae22c090cc5266015b1cf717cfc4d1b6e29c1047
zara <zara59@msn.com>
2016-02-16 10:41:40 +0100

    debut du seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Car.destroy_all
+User.destroy_all
+
+
+
+users.cars_attributes = [
+ { 
+ 		email: "toto@gmail.com",
+ 		password: "12345",
+
+ },
+
+ {
+ 		email: "tata@gmail.com",
+ 		password: "34567,"
+
+ }
+
+ {
+ 		email: "titi@gmail.com",
+ 		password: "45678,"
+
+ }
+
+]
+
+
+cars_attributes = [
+  {
+    model:         "clio",
+    brand:      "Renault",
+    description:  "tres bien",
+    price:        30,
+    address: 			"Paris",
+    engin:  			"essence",
+    year: 				2002,
+    km:   				150 000,
+    user_id: 							,
+  },
+  {
+    model:         "supra",
+    brand:      "toyota",
+    description:  "bonne etat ",
+    price:        45,
+    address: 			"lyon",
+    engin:  			"essence",
+    year: 				2007,
+    km:   				110 000,
+    user_id: 							,
+  },
+  {
+    model:         "prius",
+    brand:      "toyota",
+    description:  "neuve",
+    price:        70,
+    address: 			"Paris",
+    engin:  			"diesel",
+    year: 				2013,
+    km:   				20 000,
+    user_id: 							,
+  }
+
+]
+cars_attributes.each { |params| CAR.create!(params) }
