6a1b88491cf5a4bb32f57618817886e927514dec
zara <zara59@msn.com>
2016-02-17 11:39:04 +0100

    add downcase search

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,7 +2,7 @@ class CarsController < ApplicationController
   before_action :set_cars, only: [:show, :edit, :update, :destroy]
 
   def index
-  	@cars = Car.select{|c| c.address == params[:address]}
+  	@cars = Car.select{|c| c.address.downcase == params[:address].downcase}
   end
 
   def show
